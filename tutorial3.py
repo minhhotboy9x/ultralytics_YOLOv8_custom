2576c4577f5da3c862fb729e5f5ee674c0255276
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-07-13 11:45:26 +0700

    change

--- tutorial3.py
+++ tutorial3.py
@@ -4,6 +4,7 @@ model = YOLO("runs/detect/train3/weights/last.pt")  # or a segmentation model .i
 
 #inputs = 'datasets/AIC-HCMC-2020/images/val'  # list of numpy arrays
 # results = model.val('ultralytics/datasets/AIC-HCMC-2020.yaml')  # generator of Results objects
+# python yolov8_KD/KD_training_ver2.py --model KD/train2/weights/last.pt --teacher yolov8m.pt --resume True
 
 if __name__ == "__main__":
     # model.train(data='coco_minitrain.yaml', batch=4, epochs=200, imgsz=640, device=1, workers=2)
