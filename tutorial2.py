214794f3eb77f4b1e65e1df2ed45daa3b004e7ff
minhhotboy9x <minhnguyenquocnhat@gmail.com>
2023-07-20 17:26:55 +0700

    asset

--- tutorial2.py
+++ tutorial2.py
@@ -3,15 +3,12 @@ import torch.nn as nn
 from ultralytics import YOLO
 from ultralytics.yolo.utils import yaml_load, LOGGER, RANK
 
-# model = YOLO("yolov8n.pt")
+# model = YOLO('yolov8s.pt') #train 0
+# model = YOLO('yolov8m.pt') #train 1
+# model = YOLO('runs/detect/train/weights/last.pt') #train 0
+model = YOLO('runs/detect/train2/weights/last.pt') #train 1
 
-
-x = torch.tensor([2.0], requires_grad=True)
-y = torch.tensor([3.0])
-
-z = x.detach() + y
-
-print(z)  # Kết quả: tensor([6.])
-
-z.backward()  # Tính gradient của z
-print(x.grad)  # Kết quả: tensor([3.])
+if __name__ == '__main__':
+    # model.train(data = 'AIC-HCMC-2020.yaml', epochs = 300, batch=32, device = 0) #train0
+    # model.train(data = 'AIC-HCMC-2020.yaml', epochs = 300, batch=16, device = 1) #train1
+    model.train(resume=True)
--- tutorial3.py
+++ tutorial3.py
@@ -1,11 +1,17 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train3/weights/last.pt")  # or a segmentation model .i.e yolov8n-seg.pt
+# model = YOLO("runs/detect/train3/weights/last.pt")  # or a segmentation model .i.e yolov8n-seg.pt
+# model = YOLO("yolov8m.pt")  # or a segmentation model .i.e yolov8n-seg.pt
+model = YOLO("yolov8s.pt")  # or a segmentation model .i.e yolov8n-seg.pt
 
 #inputs = 'datasets/AIC-HCMC-2020/images/val'  # list of numpy arrays
 # results = model.val('ultralytics/datasets/AIC-HCMC-2020.yaml')  # generator of Results objects
 # python yolov8_KD/KD_training_ver2.py --model KD/train2/weights/last.pt --teacher yolov8m.pt --resume True
+# python yolov8_KD/KD_training_ver2.py --model yolov8s.pt --teacher yolov8m.pt --data coco.yaml --batch 8 --epochs 200
 
 if __name__ == "__main__":
     # model.train(data='coco_minitrain.yaml', batch=4, epochs=200, imgsz=640, device=1, workers=2)
-    model.train(resume=True)
+    # model.train(data='AIC-HCMC-2020.yaml', batch=4, epochs=200, imgsz=640, device=0, workers=2) #train 4 8m
+    # model.train(data='AIC-HCMC-2020.yaml', batch=4, epochs=200, imgsz=640, device=1, workers=2) #train 5 8s
+    model.val(data='coco_minitrain.yaml')
+    # model.train(resume=True)
